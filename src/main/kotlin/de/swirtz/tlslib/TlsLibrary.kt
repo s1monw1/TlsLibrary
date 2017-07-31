34f29bff13b4bb3557f3826f0ea38eb9cef620e0
SIM <simon.wirtz@n-design.de>
2017-07-31 13:00:43 +0200

    laden von stores

--- src/main/kotlin/de/swirtz/tlslib/TlsLibrary.kt
+++ src/main/kotlin/de/swirtz/tlslib/TlsLibrary.kt
@@ -8,18 +8,19 @@ fun main(args: Array<String>) {
     val fac = socketFactory {
         keyManager {
             algorithm = "SunX509"
-            storeFile = Paths.get("src/main/resources/mykeystore.x")
-            password = "123456"
-            fileType = "jks"
+            storeFile = Paths.get("src/main/resources/koco-ps1.p12")
+            password = "12345678"
+            fileType = "pkcs12"
         }
         trustManager {
             algorithm = "SunX509"
-            storeFile = Paths.get("src/main/resources/truststore.x")
+            storeFile = Paths.get("src/main/resources/server_truststore.jks")
             password = "123456"
             fileType = "jks"
         }
     }
 
+    //TODO Creation of socket has to be extended
     val socket = fac.createSocket("www.kotlinlang.org", 443)
     println("Connected: ${socket.isConnected}")
 }
