95ed7cbc2866492e49a9f8e4e9e839dc5f78c14e
Simon <simon.wirtzq@gmail.com>
2018-03-05 21:40:01 +0100

    refactored testserver

--- src/main/kotlin/de/swirtz/sekurity/samples/TLSServer.kt
+++ src/main/kotlin/de/swirtz/sekurity/samples/TLSServer.kt
@@ -1,8 +1,9 @@
 package de.swirtz.sekurity.samples
 
-import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.launch
+import org.slf4j.LoggerFactory
 import java.io.DataInputStream
+import java.net.ServerSocket
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.net.ServerSocketFactory
 
@@ -10,29 +11,40 @@ import javax.net.ServerSocketFactory
  * Creates Socket with supplied socketFactory and waits for a client to connect
  */
 class TLSServer(private val port: Int, private val socketFactory: ServerSocketFactory) {
+    private val LOG = LoggerFactory.getLogger(TLSServer::class.java)
 
     private var read = StringBuilder()
 
     private val running = AtomicBoolean(true)
+    private lateinit var socket: ServerSocket
 
     fun getLastMsg() = read.toString()
 
-    fun start() = launch(CommonPool) {
-        while (running.get()) {
-            val accept = socketFactory.createServerSocket(port).accept()
-            println("accepted socket $accept")
-            DataInputStream(accept.getInputStream()).use { d ->
-                var readUTF = d.readUTF()
-                while (readUTF != null) {
-                    println("Read: '$readUTF'")
-                    read.append(readUTF)
-                    readUTF = d.readUTF()
+    fun start() {
+        socket = socketFactory.createServerSocket(port)
+        LOG.debug("started server on $port")
+        launch {
+            while (running.get()) {
+                LOG.debug("wait for client to connect")
+                with(socket.accept()) {
+                    LOG.debug("accepted socket $this")
+                    DataInputStream(getInputStream()).use { d ->
+                        var readUTF = d.readUTF()
+                        while (readUTF != null) {
+                            LOG.debug("Read: '$readUTF'")
+                            read.append(readUTF)
+                            readUTF = d.readUTF()
+                        }
+                    }
                 }
             }
         }
     }
 
-    fun stop() = running.set(false)
+    fun stop() {
+        running.set(false)
+        socket.close()
+    }
 
 
 }
